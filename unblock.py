6225799737caf18d01c245ba2df2623eb9a62203
yinmo-public <yinmo.contact@gmail.com>
2019-02-10 15:46:26 +0800

    Delete unblock.py

--- unblock.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from linepy import *
-yinmo = LINE ()
-oepoll = OEPoll(yinmo)
-for cids in yinmo.getBlockedContactIds():
-    yinmo.unblockContact(cids)
-    print("已解除封鎖1位好友")
