19e8e86737d4a03df63c3054413f80765b0b84fe
yinmo-public <yinmo.contact@gmail.com>
2019-06-12 11:44:59 +0800

    Update unblock-en.py

--- unblock-en.py
+++ unblock-en.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from time import sleep
 yinmo = LINE ()
 int1 = len(yinmo.getBlockedContactIds())
 if int1 == 0:
@@ -7,6 +8,7 @@ if int1 == 0:
 else:
     for cids in yinmo.getBlockedContactIds():
         print("Unblock " + yinmo.getContact(cids).displayName)
+        sleep(0.5)
         yinmo.unblockContact(cids)
     print("\nYou unblock" + str(int1) + "friends")
     
